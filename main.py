61e699f0e401ab9df7fbe4e93db6032bb91e5562
priyanshuu7 <rajakpriyanshu581@gmail.com>
2026-06-12 20:12:34 +0530

    ..

--- main.py
+++ main.py
@@ -6,20 +6,7 @@ app  = FastAPI()
 #  home route
 @app.get("/")
 def greet():
-    return "Hello, World!"
-
-Products = [
-    {"id": 1, "name": "Laptop", "price": 999.99, "quantity": 10, "description": "A high-performance laptop."},
-    {"id": 2, "name": "Smartphone", "price": 499.99, "quantity": 20, "description": "A powerful smartphone with a great camera."},
-    {"id": 3, "name": "Headphones", "price": 199, "quantity": 30, "description": "A pair of headphones for listening to music."},
-]
+    return "This is my landing page"
 
 
-@app.get("/products")
-def get_products():
-    return Products
 
-
-@app.get("/products/{product_id}")
-def get_product(product_id: int):
-    return Products[product_id - 1]
